159013d29ff3812d4c6a948cb38a33adee781966
iamkiyotaka <erviana.205025@mhs.its.ac.id>
2021-09-26 02:27:36 +0700

    feat: import component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Routes from './routes';
+import './css/App.css';
+import Navbar from './component/Navbar';
+//import Card from './component/Card';
+import Cardcontainer from './component/Cardcontainer';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Cardcontainer />
+      <Routes />
     </div>
   );
 }
